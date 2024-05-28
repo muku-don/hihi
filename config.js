2d430be941adc7fb5b05d495078de06950b5db38
Rohan Saini <aw20010119@gmail.com>
2024-05-28 22:50:29 +0530

    boom

---
+++ config.js
@@ -0,0 +1,13 @@
+
+var config = {
+    lines: [
+        "Dear Muku 🤍,",
+        "i never knew how much you mean to me when we first started talking . until today,i still love you as much as i did since the very first day we met. ",
+        " every since you came into my life, i felt like i was the happiest. i just want to let you know how much you mean to me and how much i appreciate you.",
+        "you never failed to make me smile. things aren't getting better every since we fought, i just miss you so much. having random break downs, with you on my mind wondering if you're doing fine. im sorry i wasn't what you wanted,",
+        "im sorry i wasn't the best, im sorry i couldn't be good enough for you.i just hope everything will be okay soon, i really miss how we used to talk.😁", 
+       
+    ],
+    date:"2023-05-29T21:22:00",
+    names: ["☃︎【 Your Rohu 😛 】","‎ "]
+}
